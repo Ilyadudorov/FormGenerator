022d132be6d5c2f53a101f3e9ea404c38150bdf6
Ilyadudorov <ilyadudorov018@gmail.com>
2021-10-17 23:15:34 +0400

    Добавьте файлы проекта.

---
+++ FormGenerator/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using FormGenerator.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public Form formresult;
+
+        public string tmp;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(List<IFormFile> files)
+        {
+
+            //if (files == null)
+            //{
+            //    return Ok("NULL");
+            //}
+
+            //using (StreamReader st = new StreamReader())
+            //{
+
+            //}
+
+            //var filePaths = Path.GetFullPath(files);
+            long size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    // full path to file in temp location
+                    var filePath = Path.GetTempFileName(); //we are using Temp file name just for the example. Add your own file path.
+                    filePaths.Add(filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            tmp = filePaths.ToString();
+            return Ok(new { count = files.Count, size, filePaths });
+            //return Ok(filePaths);
+
+        }
+
+        public IActionResult Result()
+        {
+            //using (StreamReader r = new StreamReader("C:\\Users\\User\\Desktop\\formjson.json"))
+            //{
+            //    string json = r.ReadToEnd();
+            //    formresult = JsonConvert.DeserializeObject<Form>("C:\\Users\\User\\Desktop\\formjson.json");
+            //}
+            //return View(formresult);
+            
+
+            formresult = JsonConvert.DeserializeObject<Form>("C:\\Users\\User\\Desktop\\formjson.json");
+
+            return View(formresult);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FormGenerator/Models/Form.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormGenerator.Models
+{
+
+    //public class ValidationRule
+    //{
+    //    [JsonProperty("type")]
+    //    public string Type { get; set; }
+    //}
+
+    public class Attributes
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        //[JsonProperty("name")]
+        //public string Name { get; set; }
+        //[JsonProperty("placeholder")]
+        //public string Placeholder { get; set; }
+        //[JsonProperty("required")]
+        //public bool? Required { get; set; }
+        //[JsonProperty("value")]
+        //public string Value { get; set; }
+        //[JsonProperty("label")]
+        //public string Label { get; set; }
+        //[JsonProperty("class")]
+        //public string Class { get; set; }
+        //[JsonProperty("validationRules")]
+        //public List<ValidationRule> ValidationRules { get; set; }
+        //[JsonProperty("disabled")]
+        //public bool? Disabled { get; set; }
+        //[JsonProperty("checked")]
+        //public bool? Checked { get; set; }
+    }
+
+    public class Item
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("attributes")]
+
+        public Attributes Attributes { get; set; }
+    }
+
+    public class Form
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("postmessage")]
+        public string Postmessage { get; set; }
+        [JsonProperty("items")]
+        public List<Item> Items { get; set; }
+    }
+
+    //public class Root
+    //{
+    //    public Form Form { get; set; }
+    //}
+}
